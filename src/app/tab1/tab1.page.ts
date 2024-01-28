ca467845106f2d492872a6deffd9a2f553333a41
DydiRah <124720797+DydiRah@users.noreply.github.com>
2024-01-28 16:40:49 +0300

    2024-01-28

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GeneraliserService } from '../Service/generaliser.service';
 
 @Component({
   selector: 'app-tab1',
@@ -8,5 +9,23 @@ import { Component } from '@angular/core';
 export class Tab1Page {
   data: any[] = [];
 
-  constructor() {}
+  constructor(
+    private generaliserService: GeneraliserService
+  ) {
+  }
+  
+  async ngOnInit() {
+    await this.getMyAnnonce();
+  }
+
+  async getMyAnnonce() {
+    try {
+      var idAnnonceur = localStorage.getItem('CarsalidPersonne');
+      const response = await this.generaliserService.getAll('annonces/myannonce?idUser=' + idAnnonceur);
+      this.data = response;
+    } catch (error) {
+      alert(error);
+    }
+  }
+  
 }
